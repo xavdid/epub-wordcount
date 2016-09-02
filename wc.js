fc6b74c031974e3e8f03d8fe6418b850ad74c03a
David Brownman <beamneocube@gmail.com>
2016-09-02 16:14:49 -0700

    pretty decent stripped copy

--- wc.js
+++ wc.js
@@ -6,14 +6,22 @@ const EPub = require('epub')
 
 // var promisify = require('bluebird-events')
 
+const ignoredTitlesRegex = /acknowledgment|copyright|cover|dedication|title|author|contents/i
+// match all html tags, no matter their contents
 const htmlRegex = /(<([^>]+)>)/ig
-const cssRegex = /<style[^>]*>[\s\S]*<\/style>/g
 let total = 0
 
-const title = 'Rothfuss - The Name of the Wind (2007).epub'
+// const title = 'Cline - Ready Player One (2011).epub'
+// const title = 'Rothfuss - The Name of the Wind (2007).epub'
+const title = 'Gaiman - American Gods (2002).epub'
+// const title = 'Priest - The Adjacent (2013).epub'
 
 let epub = new EPub(`/Users/david/Dropbox/Ebooks/Singles/${title}`)
-epub.parse()
+try {
+  epub.parse()
+} catch (e) {
+  console.log(e.message)
+}
 // promisify(epub, {resolve: 'end'})
 
 // epub.parse().then(() => {
@@ -26,19 +34,18 @@ epub.on('end', function () {
 
 function cleanText (text) {
   let res = text
-    .replace(cssRegex, '')
     // these are replaced by spaces so that newlines in the text are properly tokenized
     .replace(htmlRegex, ' ')
     .replace(/[\n\t\r]/g, ' ')
     .split(' ')
     .filter(x => x.trim() !== '')
-  console.log(res)
+  // console.log(res)
   return res
 }
 
 function idToText (id) {
   return new Promise(function (resolve, reject) {
-    epub.getChapterRaw(id, function (err, text) {
+    epub.getChapter(id, function (err, text) {
       if (err) { throw (err) }
       resolve(cleanText(text).length)
     })
@@ -47,12 +54,21 @@ function idToText (id) {
 
 function main () {
   Promise.all(epub.flow.map((chapter) => {
-    return idToText(chapter.id).then(function (res) {
-      // console.log(res)
-      total += res
-    })
+    // console.log(chapter.title)
+    // if title exists, make sure it's not an ignored page
+    if (chapter.title === undefined || !chapter.title.match(ignoredTitlesRegex)) {
+      return idToText(chapter.id).then((res) => {
+        // console.log(res)
+        total += res
+      // console.log(`word count: ${total}`)
+      })
+    } else {
+      // skip it!
+      return Promise.resolve()
+    }
   })).then(() => {
     // all done!
+    console.log(`${epub.metadata.title}:`)
     console.log(`word count: ${total}`)
   }).catch((err) => {
     console.log(err)
